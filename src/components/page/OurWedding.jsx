c3e47df0849fa9efbba6f72ba6072850cd24fbde
joeyhage <joecool520@gmail.com>
2019-05-06 20:29:33 -0500

    Fixed home page hero issue with IE and Edge

--- src/components/page/OurWedding.jsx
+++ src/components/page/OurWedding.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import camera from '../../resources/our-wedding-page/camera.svg';
 import church from '../../resources/our-wedding-page/church.svg';
 import gift from '../../resources/our-wedding-page/gift.svg';
+import images from '../../resources/our-wedding-page/images';
 import rsvp from '../../resources/our-wedding-page/rsvp.png';
 
 const QuickLink = props => {
@@ -25,16 +26,21 @@ const QuickLink = props => {
 
 class OurWedding extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { heroImage: null };
+    this.determineHeroImage = this.determineHeroImage.bind(this);
+  }
+
   render() {
     return (
       <div className='is-clipped' id='our-wedding-page'>
         <Helmet>
           <title>Abby Hanson and Joey Hage&apos;s Wedding</title>
-          <meta name='description' content='Abby Hanson and Joey Hage are getting married! The wedding will be held on November 9th, 2019 in West Des Moines, IA. #ToHageAndToHold' />
+          <meta name='description'
+                content='Abby Hanson and Joey Hage are getting married! The wedding will be held on November 9th, 2019 in West Des Moines, IA. #ToHageAndToHold' />
         </Helmet>
-        <img
-          src='https://lh3.googleusercontent.com/yHXM9SuLystaGBXG5WZA8X2sTJyuD4VymTRcNuZx0AzZ-J4ZcOt0qNy1CedW0YdbCD_HZ9ofvSYiyE7yTK4hdsFwMuqlnw1Qyi-mK16Ovh827Q9fstzoNUE6zNTNtsBLDC-Sr0pwAeMQCdBhvyqXST5dOudGt56jjhl5zeDVNhcjmJgnV_RdEj592WKOpGNrWx6zHs4zYGZc-rujy8Iqfslo7dU-d6WW1qxZtEfdCgA8YecMavKXSPdkSbezbffOSi63E5tojNi4THvGeewTMBPnPO5HbLeoXb77a3QuqIpRLwYiVFpteeOOCjYJCsocHhlqwBn6x_JE6U2VeB_eXuSBoe424EuCzr7QIz8W1M2mDVtpcoznd7rAeIvkp8XxI7LqU5vxLbdRqr0GVo3_dn8FsEHo0frjkBn8SLDm86__8zVYHT8Ae57nngDL15CmT2E37fKw2vzyc5vd5sCsWG8b5otxuXS_zU57ZAE5SD03FWMk6o7bNtdvGcEhW5RxKjGL5qzn08ylqo8DXsxypi2-D4FNL-Mq7jiPWO7uc4qXNZ5GyvV1Wwv55maiGw3fMavKKJLhkwEcjxF_f5EfJBgCPvSb6C7_wZJxHHlaMtrsaC0Rt7Utd7RHoQLK3vMFPnmawYs8nV4AaYsHyggfxBWmpHNr3SyQ7LxlBp7l0VNCRsZVN4pHQG5vNMdSv-negdh9TjEoi8k1lKOUOCXCdyeE=w1420-h947-no'
-          alt='Abby and Joey' />
+        <img src={this.state.heroImage} alt='Abby and Joey' />
         <div className='burgundy'>
           <h2 className='is-size-3 great-vibes'>
             We're Getting Married!
@@ -61,6 +67,20 @@ class OurWedding extends Component {
         </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.determineHeroImage();
+    window.addEventListener('resize', this.determineHeroImage);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.determineHeroImage);
+  }
+
+  determineHeroImage() {
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    this.setState({ heroImage: (mediaQuery && mediaQuery.matches) ? images.mobile : images.tablet });
   }
 }
 
